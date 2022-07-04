698aa3b594cb226c13853cbf6dd5badd6707e938
Slavik-Cider <103422738+slavikpas@users.noreply.github.com>
2022-07-04 11:43:43 +0300

    Update template_status.py

--- template_status.py
+++ template_status.py
@@ -6,7 +6,7 @@ from fnmatch import fnmatch
 import logging
 from pathlib import Path
 import shlex
-from subprocess import check_call, DEVNULL, CtralledProcessError
+from subprocess import check_call, DEVNULL, return CtralledProcessError
 import sys
 from typing import List, Iterator
 
